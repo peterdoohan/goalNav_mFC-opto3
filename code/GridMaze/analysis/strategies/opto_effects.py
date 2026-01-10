ced787f6bc77825c9dd5c6cdcb4f836c471e5888
Peter Doohan <peterdoohan0@gmail.com>
2026-01-10 02:24:23 +0000

    updating summary notebook with navigation msm results (including habits)

--- code/GridMaze/analysis/strategies/opto_effects.py
+++ code/GridMaze/analysis/strategies/opto_effects.py
@@ -24,13 +24,36 @@ MAX_STIM_DURATION = 30  # seconds
 # %%%
 
 
+def plot_mixture_of_strategy_weights(
+    results_df,
+    cmap="tab10",
+    print_stats=True,
+    axes=None,
+):
+    """ """
+    strategies = [c for c in results_df.columns if c not in ["subject_ID", "condition", "stim_trial"]]
+    if axes is None:
+        fig, axes = plt.subplots(1, len(strategies), figsize=(2 * len(strategies), 3.5))
+    for ax in axes:
+        ax.axhline(0, color="k", ls="--", alpha=0.5)
+
+    colors = sns.color_palette(cmap, len(strategies))
+    for strategy, color, ax in zip(strategies, colors, axes):
+        cp.plot_group_by_stim(
+            results_df,
+            y=strategy,
+            ax=ax,
+            stim_color=color,
+            print_stats=print_stats,
+        )
+
+
 def get_group_by_stim_strategy_weights(
     navigation_strategies_df,
     strategies=["vector", "structure", "habit", "backtracking_penalty"],
     stim_day_range=(6, gs.TOTAL_STIM_DAYS),
     max_trial_duration=None,
     stim_only=True,
-    quick_plot=True,
 ):
     """ """
     # filter data
@@ -65,14 +88,6 @@ def get_group_by_stim_strategy_weights(
                 }
             )
     results_df = pd.DataFrame(results)
-    if quick_plot:
-        for y in strategies:
-            cp.plot_group_by_stim(
-                results_df,
-                y,
-                print_stats=True,
-                stim_color="orange",
-            )
     return results_df
 
 
