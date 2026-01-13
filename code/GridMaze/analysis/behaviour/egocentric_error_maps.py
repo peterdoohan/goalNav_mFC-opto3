2408afd0d9220fe148cc6c1f431efeceffdb7197
Peter Doohan <peterdoohan0@gmail.com>
2026-01-13 04:17:12 +0000

    new script for egocentric error maps, slowly port over stuff from error.py

---
+++ code/GridMaze/analysis/behaviour/egocentric_error_maps.py
@@ -0,0 +1,11 @@
+"""
+visualize egocentric error maps,
+i.e. where was the goal relative to subject POV when they make navigational errors
+@peterdoohan
+"""
+
+# %% Imports
+
+# %% Global Variables
+
+# %% Function
