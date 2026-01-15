708b80cafbe34081ee8a8d781f59931ef658565b
Peter Doohan <peterdoohan0@gmail.com>
2026-01-15 07:40:44 +0000

    fixed bug in defining correct actions

--- code/GridMaze/analysis/strategies/psychometrics.py
+++ code/GridMaze/analysis/strategies/psychometrics.py
@@ -6,6 +6,7 @@ Make some psycometric curves for P(correct) vs Habit Values
 import json
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 
 from GridMaze.analysis.core import get_sessions as gs
 from GridMaze.analysis.strategies import get_input_data as gid
@@ -22,10 +23,45 @@ MAX_STIM_DURATION = 30  # seconds
 # %% Functions
 
 
+def plot_habit_psychometrics_summary(psy_curve_df, stim_color="#0077FF", axes=None):
+    """ """
+    if axes is None:
+        f, axes = plt.subplots(1, 2, figsize=(4, 2.5), sharey=True, sharex=True)
+    for ax in axes:
+        ax.spines[["top", "right"]].set_visible(False)
+        ax.set_xlabel("correct \n habit value")
+        ax.axvline(0.5, color="k", ls="--", alpha=0.5)
+    axes[0].set_ylabel("P(correct)")
+
+    grouped_df = psy_curve_df.groupby(["condition", "stim_trial", "habit_value"], observed=True).p_correct
+    mean = grouped_df.mean()
+    sem = grouped_df.sem()
+    for ax, cond in zip(axes, ["control", "opto"]):
+        for stim_trial, color in zip([False, True], ["dimgrey", stim_color]):
+            _mean = mean.loc[(cond, stim_trial)]
+            _sem = sem.loc[(cond, stim_trial)]
+            ax.errorbar(
+                _mean.index,
+                _mean.values,
+                yerr=_sem.values,
+                label="light on" if stim_trial else "light off",
+                color=color,
+                marker="o",
+                markersize=5,
+                linestyle="-",
+                alpha=0.8,
+            )
+            ax.set_title(cond)
+        ax.set_ylim(0, 1.1)
+    axes[0].legend(fontsize="x-small")
+
+
 def get_psychometrics_df(
     navigation_strategies_df,
     stim_day_range=(4, gs.TOTAL_STIM_DAYS),
     stim_only=True,
+    x="habit",
+    x_bins=6,
 ):
     """ """
     # filter data
@@ -35,19 +71,24 @@ def get_psychometrics_df(
     if stim_only:
         df = df[df.time_in_trial.le(MAX_STIM_DURATION)]
 
+    dfs = []
     for subject in SUBJECT_IDS:
         for stim_trial in [True, False]:
-            subj_df = df[(df.subject_ID == subject) & (df.stim_trial == stim_trial)]
-            return subj_df
-            condition = subj_df.condition.unique()[0]
+            subj_df = df[(df.subject_ID == subject) & (df.stim_trial == stim_trial)].copy()
+            psy_curve = get_psychometric_curve(subj_df, x=x, bins=x_bins)
+            psy_curve["subject_ID"] = subject
+            psy_curve["condition"] = subj_df.condition.unique()[0]
+            psy_curve["stim_trial"] = stim_trial
+            dfs.append(psy_curve)
 
-    return
+    psy_curve_df = pd.concat(dfs, ignore_index=True)
+    return psy_curve_df
 
 
 def get_psychometric_curve(df, x="habit", bins=5):
     """ """
     # add column for if subject choice == y
-    df[("correct", "")] = df.subject_choice.eq(df.optimal_action).all(axis=1)
+    df[("correct", "")] = ((df.subject_choice == 1) & (df.optimal_action == 1)).any(axis=1)
     # get x value for correct action (handeling for multiple optimal actions)
     x_probs = df[x].values
     opt_mask = df.optimal_action.values.astype(bool)
