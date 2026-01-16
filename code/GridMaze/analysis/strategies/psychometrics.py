7a146c42f6c8f0166572f137d335dce0040d4fd0
Peter Doohan <peterdoohan0@gmail.com>
2026-01-16 01:09:37 +0000

    added sigmoid curve fit to psycho curves

--- code/GridMaze/analysis/strategies/psychometrics.py
+++ code/GridMaze/analysis/strategies/psychometrics.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from pingouin import mixed_anova
+from scipy.optimize import curve_fit
 
 
 from GridMaze.analysis.core import get_sessions as gs
@@ -25,8 +26,72 @@ MAX_STIM_DURATION = 30  # seconds
 # %% Functions
 
 
-def plot_habit_psychometrics_summary(psy_curve_df, stim_color="#0077FF", print_stats=True, axes=None):
+def get_curve_fit_df(psy_curve_df):
+    fit_results = []
+    for subject in SUBJECT_IDS:
+        for stim_trial in [True, False]:
+            sub_df = psy_curve_df[(psy_curve_df.subject_ID == subject) & (psy_curve_df.stim_trial == stim_trial)]
+            x = sub_df.habit_value.values.astype(float)
+            y = sub_df.p_correct.values.astype(float)
+            fit_params = get_sigmoid_curve(x, y, return_as="params")
+            fit_params["subject_ID"] = subject
+            fit_params["stim_trial"] = stim_trial
+            fit_params["condition"] = sub_df.condition.unique()[0]
+            fit_results.append(fit_params)
+    fit_df = pd.DataFrame(fit_results)
+    return fit_df
+
+
+def plot_habit_psychometrics_inset(
+    psy_curve_df,
+    group="opto",
+    habit_value_range=(0, 0.25),
+    stim_color="#0077FF",
+    ax=None,
+):
+    # set up fig
+    if ax is None:
+        f, ax = plt.subplots(1, 1, figsize=(0.75, 1.5))
+    ax.spines[["top", "right"]].set_visible(False)
+    ax.set_xlabel("")
+    ax.set_ylabel("")
+
+    df = psy_curve_df.copy()
+    df["habit_value"] = df.habit_value.astype(float)
+    df = df[df.habit_value.between(*habit_value_range)]
+    grouped_df = df.groupby(["condition", "stim_trial", "habit_value"], observed=True).p_correct
+    mean = grouped_df.mean()
+    sem = grouped_df.sem()
+
+    # plot
+    for stim_trial, color in zip([False, True], ["dimgrey", stim_color]):
+        # plot error bars
+        _mean = mean.loc[(group, stim_trial)]
+        _sem = sem.loc[(group, stim_trial)]
+        ax.errorbar(
+            _mean.index,
+            _mean.values,
+            yerr=_sem.values,
+            fmt="o",
+            label="light on" if stim_trial else "light off",
+            color=color,
+            markersize=4,
+            alpha=1,
+        )
+        ax.set_title(group)
+    ax.set_xlim(*habit_value_range)
+
+    return
+
+
+def plot_habit_psychometrics_summary(
+    psy_curve_df,
+    stim_color="#0077FF",
+    print_stats=True,
+    axes=None,
+):
     """ """
+    # set up fig
     if axes is None:
         f, axes = plt.subplots(1, 2, figsize=(4, 2.5), sharey=True, sharex=True)
     for ax in axes:
@@ -40,19 +105,23 @@ def plot_habit_psychometrics_summary(psy_curve_df, stim_color="#0077FF", print_s
     sem = grouped_df.sem()
     for ax, cond in zip(axes, ["control", "opto"]):
         for stim_trial, color in zip([False, True], ["dimgrey", stim_color]):
+            # plot error bars
             _mean = mean.loc[(cond, stim_trial)]
             _sem = sem.loc[(cond, stim_trial)]
             ax.errorbar(
                 _mean.index,
                 _mean.values,
                 yerr=_sem.values,
+                fmt="o",
                 label="light on" if stim_trial else "light off",
                 color=color,
-                marker="o",
-                markersize=5,
-                linestyle="-",
-                alpha=0.8,
+                markersize=4,
+                alpha=1,
             )
+            # plot fitted sigmoid curve
+            x, y = _mean.index.values.astype(float), _mean.values.astype(float)
+            x_fit, y_fit = get_sigmoid_curve(x, y)
+            ax.plot(x_fit, y_fit, color=color)
             ax.set_title(cond)
         ax.set_ylim(0, 1.1)
 
@@ -73,8 +142,9 @@ def plot_habit_psychometrics_summary(psy_curve_df, stim_color="#0077FF", print_s
 
 def get_psychometrics_df(
     navigation_strategies_df,
-    stim_day_range=(6, gs.TOTAL_STIM_DAYS),
+    stim_day_range=(4, gs.TOTAL_STIM_DAYS),
     stim_only=True,
+    decision_points_only=False,
     x="habit",
     x_bins=8,
 ):
@@ -85,6 +155,9 @@ def get_psychometrics_df(
         df = df[df.total_stim_days.between(*stim_day_range)]
     if stim_only:
         df = df[df.time_in_trial.le(MAX_STIM_DURATION)]
+    if decision_points_only:
+        # nodes with degree 3 or 4
+        df = df[df.available.sum(axis=1).gt(2)]
 
     dfs = []
     for subject in SUBJECT_IDS:
@@ -127,3 +200,32 @@ def get_data():
         n_history=1,
         verbose=False,
     )
+
+
+# %% sigmoid curve fitting
+
+
+def get_sigmoid_curve(x, y, return_as="line"):
+    p0 = [0, 10.0, -100, 0.05]  # alpha  # beta  # gamma  # lambda
+    bounds = ([-np.inf, 0, -np.inf, -np.inf], [np.inf, np.inf, 0, np.inf])
+    # curve fit
+    popt, pcov = curve_fit(sigmoid_4p, x, y, p0=p0, bounds=bounds, maxfev=100_000)
+    # generate fit line
+    x_fit = np.linspace(0, 1, 500)
+    y_fit = sigmoid_4p(x_fit, *popt)
+    if return_as == "line":
+        return x_fit, y_fit
+    elif return_as == "params":
+        return {"alpha": popt[0], "beta": popt[1], "gamma": popt[2], "lambda": popt[3]}
+    else:
+        raise ValueError("return_as must be 'line' or 'params'")
+
+
+def sigmoid_4p(x, alpha, beta, gamma, lam):
+    """
+    alpha : threshold (inflection point)
+    beta  : slope
+    gamma : lower asymptote (guess rate)
+    lam   : lapse rate (upper asymptote = 1 - lam)
+    """
+    return gamma + (1 - gamma - lam) / (1 + np.exp(-beta * (x - alpha)))
