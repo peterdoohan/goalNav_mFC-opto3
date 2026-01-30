192531bde9849fdfab24f003bc10749fea1161aa
Peter Doohan <peterdoohan0@gmail.com>
2026-01-30 17:04:40 +0000

    new analyses to corrleate inhibited anatomy with behaviour

---
+++ code/GridMaze/analysis/anatomy/corr_behaviour.py
@@ -0,0 +1,20 @@
+"""
+Correlate features from inhibited anatomy with behaviour deficits.
+"""
+
+# %% Imports
+import numpy as np
+import pandas as pd
+
+from GridMaze.analysis.anatomy import blobs as ab
+from GridMaze.analysis.behaviour import performance_metrics as pm
+from GridMaze.analysis.strategies import get_input_data as gid
+from GridMaze.analysis.strategies import opto_effects as oe
+
+# %% Global Variables
+
+# %% Functions
+
+
+def test():
+    return
